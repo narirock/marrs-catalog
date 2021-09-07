7ddd9664ca9ba583ca0404ee96818c5cd38f5b92
narirock <nari9rock@hotmail.com>
2021-09-07 18:18:56 -0300

    ajustando product reads

--- src/Models/ProductRead.php
+++ src/Models/ProductRead.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductRead extends Model
 {
     protected $table = "catalog_product_reads";
-    protected $fillable = ['product_id', 'ip'];
+    protected $fillable = ['catalog_product_id', 'ip'];
 
     public function products()
     {
-        return $this->belongsTo('Marrs\MarrsCatalog\Models\Product');
+        return $this->hasOne('Marrs\MarrsCatalog\Models\Product', 'catalog_product_id');
     }
 }
